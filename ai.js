96ca2b5585e130870175b09f81e048f7b056f17f
ILAN <x3voo160@gmail.com>
2022-01-21 21:08:11 +0100

    init

---
+++ ai.js
@@ -0,0 +1,320 @@
+var aiIntervalId;
+
+//term A[left] 0-49
+//term B[center] 25-50-75
+//term C[right] 51-100
+	ZbSens = [];
+	ZbSens[0] = [];
+	ZbSens[1] = []; //Blisko
+	ZbSens[2] = []; //Mid
+	ZbSens[3] = []; //Daleko
+	
+	ZbSteer = [];
+	ZbSteer[0] = [];
+	ZbSteer[1] = []; //LEFT
+	ZbSteer[2] = []; //MID
+	ZbSteer[3] = []; //RIGHT
+	
+	YR = [];
+	YR[0] = [];
+	YR[1] = [];
+	YR[2] = [];
+	YR[3] = [];
+	YR[4] = [];
+
+	
+for (i=0; i <= 100; i++){
+	ZbSens[0][i] = 0; 
+	ZbSens[1][i] = 0; //Blisko
+	ZbSens[2][i] = 0; //Mid
+	ZbSens[3][i] = 0; //Daleko
+	
+	ZbSteer[0][i] = 0;
+	ZbSteer[1][i] = 0; //LEFT
+	ZbSteer[2][i] = 0; //MID
+	ZbSteer[3][i] = 0; //RIGHT
+	
+	YR[0][i] = 0;
+	YR[1][i] = 0;
+	YR[2][i] = 0;
+	YR[3][i] = 0;
+	YR[4][i] = 0;
+}
+	
+for (i=0; i <= 100; i++){
+	x = i;
+
+	ZbSens[0][i] = x;
+	ZbSteer[0][i] = x;
+	YR[0][i] = x;
+	
+	//term A Blisko
+	if (x >= 0 && x <= 50)
+		ZbSens[1][i] = (50 - x) / (50 - 0);
+	else
+		ZbSens[1][i] = 0;
+	
+	//term B Mid
+	if (x >= 0 && x < 50)
+		ZbSens[2][i] = (x - 0) / (50 - 0);
+	else if (x >= 50 && x <= 100)
+		ZbSens[2][i] = (100 - x) / (100 - 50);
+	else
+		ZbSens[2][i] = 0;
+	
+	//term C Daleko
+	if (x > 50 && x <= 100)
+		ZbSens[3][i] = (50 - x) / (50 - 100);
+	else
+		ZbSens[3][i] = 0;
+	
+	
+	
+	//term A LEFT
+	if (x >= 0 && x <= 50)
+		ZbSteer[1][i] = (50 - x) / (50 - 0);
+	else
+		ZbSteer[1][i] = 0;
+	
+	//term B MID
+	if (x >= 0 && x < 50)
+		ZbSteer[2][i] = (x - 0) / (50 - 0);
+	else if (x >= 50 && x <= 75)
+		ZbSteer[2][i] = (100 - x) / (100 - 50);
+	else
+		ZbSteer[2][i] = 0;
+	
+	//term C RIGHT
+	if (x > 50 && x <= 100)
+		ZbSteer[3][i] = (50 - x) / (50 - 100);
+	else
+		ZbSteer[3][i] = 0;
+	
+}
+
+function monkaS(inp1, inp2, inp3) {
+	for (i=0; i <= 100; i++){
+		YR[1][i] = 0;
+		YR[2][i] = 0;
+		YR[3][i] = 0;
+		YR[4][i] = 0;
+	}
+	
+	var blisko_s1 = ZbSens[1][(inp1*2)];
+	var mid_s1 = ZbSens[2][(inp1*2)];
+	var daleko_s1 = ZbSens[3][(inp1*2)];
+	
+	var blisko_s2 = ZbSens[1][(inp2*2)];
+	var mid_s2 = ZbSens[2][(inp2*2)];
+	var daleko_s2 = ZbSens[3][(inp2*2)];
+	
+	var blisko_s3 = ZbSens[1][(inp3*2)];
+	var mid_s3 = ZbSens[2][(inp3*2)];
+	var daleko_s3 = ZbSens[3][(inp3*2)];
+	
+	for (i=0; i <= 100; i++){
+		//R1 blisko blisko blisko right
+		if (blisko_s1 > 0 & blisko_s2 > 0 & blisko_s3 > 0)
+			YR[3][i] = Math.min(blisko_s1, blisko_s2, blisko_s3, ZbSteer[3][i]); //RIGHT
+		
+		//R2 blisko blisko srednio right
+		if (blisko_s1 > 0 & blisko_s2 > 0 & mid_s3 > 0)
+			YR[3][i] = Math.min(blisko_s1, blisko_s2, mid_s3, ZbSteer[3][i]); //RIGHT
+		
+		//R3 blisko blisko daleko right
+		if (blisko_s1 > 0 & blisko_s2 > 0 & daleko_s3 > 0)
+			YR[3][i] = Math.min(blisko_s1, blisko_s2, daleko_s3, ZbSteer[3][i]); //RIGHT
+		
+		//R4 blisko srednio blisko prosto
+		if (blisko_s1 > 0 & mid_s2 > 0 & blisko_s3 > 0)
+			YR[2][i] = Math.min(blisko_s1, mid_s2, blisko_s3, ZbSteer[2][i]); //MID
+		
+		//R5 blisko srednio srednio right
+		if (blisko_s1 > 0 & mid_s2 > 0 & mid_s3 > 0)
+			YR[3][i] = Math.min(blisko_s1, mid_s2, mid_s3, ZbSteer[3][i]); //RIGHT
+		
+		//R6 blisko srednio daleko right
+		if (blisko_s1 > 0 & mid_s2 > 0 & daleko_s3 > 0)
+			YR[3][i] = Math.min(blisko_s1, mid_s2, daleko_s3, ZbSteer[3][i]); //RIGHT
+		
+		//R7 blisko daleko blisko prosto
+		if (blisko_s1 > 0 & daleko_s2 > 0 & blisko_s3 > 0)
+			YR[2][i] = Math.min(blisko_s1, daleko_s2, blisko_s3, ZbSteer[2][i]); //MID
+		
+		//R8 blisko daleko srednio prosto
+		if (blisko_s1 > 0 & daleko_s2 > 0 & mid_s3 > 0)
+			YR[3][i] = Math.min(blisko_s1, daleko_s2, mid_s3, ZbSteer[3][i]); //RIGHT
+		//PATCH: MID -> RIGHT
+		
+		//R9 blisko daleko daleko prosto
+		if (blisko_s1 > 0 & daleko_s2 > 0 & daleko_s3 > 0)
+			YR[3][i] = Math.min(blisko_s1, daleko_s2, daleko_s3, ZbSteer[3][i]); //RIGHT
+		//PATCH: MID -> RIGHT
+		
+		//R10 srednio blisko blisko left
+		if (mid_s1 > 0 & blisko_s2 > 0 & blisko_s3 > 0)
+			YR[1][i] = Math.min(mid_s1, blisko_s2, blisko_s3, ZbSteer[1][i]); //LEFT
+		
+		//R11 srednio blisko srednio left
+		if (mid_s1 > 0 & blisko_s2 > 0 & mid_s3 > 0)
+			YR[1][i] = Math.min(mid_s1, blisko_s2, mid_s3, ZbSteer[1][i]); //LEFT
+		
+		//R12 srednio blisko daleko right
+		if (mid_s1 > 0 & blisko_s2 > 0 & daleko_s3 > 0)
+			YR[3][i] = Math.min(mid_s1, blisko_s2, daleko_s3, ZbSteer[3][i]); //RIGHT
+		
+		//R13 srednio srednio blisko left
+		if (mid_s1 > 0 & mid_s2 > 0 & blisko_s3 > 0)
+			YR[1][i] = Math.min(mid_s1, mid_s2, blisko_s3, ZbSteer[1][i]); //LEFT
+		
+		//R14 srednio srednio srednio prosto
+		if (mid_s1 > 0 & mid_s2 > 0 & mid_s3 > 0)
+			YR[2][i] = Math.min(mid_s1, mid_s2, mid_s3, ZbSteer[2][i]); //MID
+		
+		//R15 srednio srednio daleko right
+		if (mid_s1 > 0 & mid_s2 > 0 & daleko_s3 > 0)
+			YR[3][i] = Math.min(mid_s1, mid_s2, daleko_s3, ZbSteer[3][i]); //RIGHT
+		
+		//R16 srednio daleko blisko left
+		if (mid_s1 > 0 & daleko_s2 > 0 & blisko_s3 > 0)
+			YR[1][i] = Math.min(mid_s1, daleko_s2, blisko_s3, ZbSteer[1][i]); //LEFT
+		
+		//R17 srednio daleko srednio prosto
+		if (mid_s1 > 0 & daleko_s2 > 0 & mid_s3 > 0)
+			YR[2][i] = Math.min(mid_s1, daleko_s2, mid_s3, ZbSteer[2][i]); //MID
+		
+		//R18 srednio daleko daleko prosto
+		if (mid_s1 > 0 & daleko_s2 > 0 & daleko_s3 > 0)
+			YR[3][i] = Math.min(mid_s1, daleko_s2, daleko_s3, ZbSteer[3][i]); //RIGHT
+		//PATCH: MID -> RIGHT
+		
+		
+		//R19 daleko blisko blisko left
+		if (daleko_s1 > 0 & blisko_s2 > 0 & blisko_s3 > 0)
+			YR[1][i] = Math.min(daleko_s1, blisko_s2, blisko_s3, ZbSteer[1][i]); //LEFT
+		
+		//R20 daleko blisko srednio left
+		if (daleko_s1 > 0 & blisko_s2 > 0 & mid_s3 > 0)
+			YR[1][i] = Math.min(daleko_s1, blisko_s2, mid_s3, ZbSteer[1][i]); //LEFT
+		
+		//R21 daleko blisko daleko left
+		if (daleko_s1 > 0 & blisko_s2 > 0 & daleko_s3 > 0)
+			YR[1][i] = Math.min(daleko_s1, blisko_s2, daleko_s3, ZbSteer[1][i]); //LEFT
+		
+		//R22 daleko srednio blisko left
+		if (daleko_s1 > 0 & mid_s2 > 0 & blisko_s3 > 0)
+			YR[1][i] = Math.min(daleko_s1, mid_s2, blisko_s3, ZbSteer[1][i]); //LEFT
+		
+		//R23 daleko srednio srednio left
+		if (daleko_s1 > 0 & mid_s2 > 0 & mid_s3 > 0)
+			YR[1][i] = Math.min(daleko_s1, mid_s2, mid_s3, ZbSteer[1][i]); //LEFT
+		
+		//R24 daleko srednio daleko left
+		if (daleko_s1 > 0 & mid_s2 > 0 & daleko_s3 > 0)
+			YR[1][i] = Math.min(daleko_s1, mid_s2, daleko_s3, ZbSteer[1][i]); //LEFT
+		
+		//R25 daleko  daleko blisko prosto
+		if (daleko_s1 > 0 & daleko_s2 > 0 & blisko_s3 > 0)
+			YR[1][i] = Math.min(daleko_s1, daleko_s2, blisko_s3, ZbSteer[1][i]); //LEFT
+		//PATCH: MID -> LEFT
+		
+		//R26 daleko daleko srednio prosto
+		if (daleko_s1 > 0 & daleko_s2 > 0 & mid_s3 > 0)
+			YR[1][i] = Math.min(daleko_s1, daleko_s2, mid_s3, ZbSteer[1][i]); //LEFT
+		//PATCH: MID -> LEFT
+		
+		//R27 daleko daleko daleko prosto
+		if (daleko_s1 > 0 & daleko_s2 > 0 & daleko_s3 > 0)
+			YR[2][i] = Math.min(daleko_s1, daleko_s2, daleko_s3, ZbSteer[2][i]); //MID
+			
+	}
+	
+	for (i=0; i <= 100; i++){
+		YR[4][i] = Math.max(YR[1][i], YR[2][i], YR[3][i]);
+	}
+	
+	var a = 0;
+	var b = 0;
+	
+	for (i=0; i <= 100; i++){
+		a = a + YR[0][i] * YR[4][i];
+		b = b + YR[4][i];
+	}
+	
+	return a/b;
+}
+
+var preCompArray = [];
+
+
+function preCompile() {
+	for (iPre=0; iPre <= 50; iPre++){
+		preCompArray[iPre] = [];
+		for (jPre=0; jPre <= 50; jPre++){
+			preCompArray[iPre][jPre] = [];
+			for (kPre=0; kPre <= 50; kPre++){
+				preCompArray[iPre][jPre][kPre] = Math.round(monkaS(iPre,jPre,kPre));
+			}
+		}
+		console.clear();
+		console.log(".".repeat(iPre));
+	}
+	console.log("done. pre-calculated "+Math.pow(preCompArray.length, 3)+" results");
+}
+
+
+var angle;
+
+function start_ai(){
+	
+	controlls("ai", true, 0);
+	
+	aiIntervalId = setInterval(function(){
+		
+		//angle = Math.round(monkaS(Math.round(car_sensor("left").d), Math.round(car_sensor("front").d), Math.round(car_sensor("right").d)));
+		
+		angle = preCompArray[Math.round(car_sensor("left").d)][Math.round(car_sensor("front").d)][Math.round(car_sensor("right").d)];
+		
+		controlls("up", true);
+		//console.log(Math.round(car_sensor("left").d)+":"+Math.round(car_sensor("front").d)+":"+Math.round(car_sensor("right").d)+":"+angle);
+		
+		if(angle == 50){
+			controlls("right", false, 0);
+			controlls("left", false, 0);
+			//console.log("UP");
+		}else if (angle < 50) {
+			controlls("left", true, 180*((angle-50)/50));
+			controlls("right", false, 0);
+			//console.log("LEFT");
+		}else if (angle > 50){
+			controlls("right", true, 180*((angle-50)/50));
+			controlls("left", false, 0);
+			//console.log("RIGHT");
+		}
+		
+
+		
+	}, 8);
+}
+
+/*
+controlls("right/left", true/false, angle)
+controlls("up/down, true/false)
+car_sensor("front")
+car_sensor("left")
+car_sensor("right")
+range = 50
+
+
+*/
+
+
+function stop_ai(){
+	clearInterval(aiIntervalId);
+	controlls("left", false);
+	controlls("right", false);
+	controlls("up", false);
+	controlls("down", false);
+	controlls("space", false);
+	controlls("ai", false);
+}
---
+++ gfx.js
@@ -0,0 +1,504 @@
+const WIDTH = 400, HEIGHT = 400;
+
+var canvas = document.getElementById("main");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#0000FF";
+ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+var cords = document.getElementById("cords");
+var draw_time = document.getElementById("draw_time");
+
+var wheelsUi = document.getElementById("wheelsUi");
+var carAngleUi = document.getElementById("carAngleUi");
+var speedUi = document.getElementById("speedUi");
+var sensLeftUi = document.getElementById("sensLeftUi");
+var sensCenterUi = document.getElementById("sensCenterUi");
+var sensRightUi = document.getElementById("sensRightUi");
+var carXposUi = document.getElementById("carXpos")
+var carYposUi = document.getElementById("carYpos")
+var carXposAbsUi = document.getElementById("carXposAbs")
+var carYposAbsUi = document.getElementById("carYposAbs")
+
+
+left = false;
+right = false;
+up = false;
+down = false;
+space = false;
+ai = false;
+customAngle = 0;
+
+var array2D = [];
+init_array2D();
+
+function init_array2D(){
+	for(var i = 0 ; i < HEIGHT; i++){
+		array2D[i] = [];
+		for(var j = 0 ; j < WIDTH; j++){
+			array2D[i][j] = "#007070";
+		}
+	}
+
+}
+
+function clear_array2D(){
+	for(var i = 0 ; i < HEIGHT; i++){
+		//array2D[i] = [];
+		for(var j = 0 ; j < WIDTH; j++){
+			array2D[i][j] = "#007070";
+		}
+	}
+
+}
+
+
+var id = ctx.getImageData(0, 0, 1, 1);
+
+playerPosX =80;
+playerPosY =80;
+playerAbsolutePosX = 80;
+playerAbsolutePosY = 80;
+
+var angle = 0;
+var absoluteAngle = 50;
+
+var wheel = 2;
+
+
+
+var i = setInterval(function(){
+	
+	playerPosX = Math.round(playerAbsolutePosX);
+	playerPosY = Math.round(playerAbsolutePosY);
+	
+	draw(true, playerPosX, playerPosY, 51);//20*(4/3) //termp !! = 30
+
+	var sa = wheel;
+	
+	angle = 0;
+	
+	if(left == true){
+		if (ai == true)
+			steer("left", customAngle);
+		else
+			steer("left", -60);
+	}
+	if(right == true)
+		if (ai == true)
+			steer("right", customAngle);
+		else
+			steer("right", 60);
+	if(up == true)
+		acc(1);
+	if(down == true)
+		acc(-1);
+
+	var sensTempFront = car_sensor("front");
+	ctx.beginPath();
+	ctx.moveTo(playerPosX, playerPosY);
+	ctx.lineTo(sensTempFront.x, sensTempFront.y);
+	ctx.lineWidth = 2;
+	// set line color
+	ctx.strokeStyle = '#808000';
+	ctx.stroke();
+	
+	sensCenterUi.innerText = "Sensor front: " + sensTempFront.d;
+	
+	var sensTempLeft = car_sensor("left");
+	ctx.beginPath();
+	ctx.moveTo(playerPosX, playerPosY);
+	ctx.lineTo(sensTempLeft.x, sensTempLeft.y);
+	ctx.lineWidth = 2;
+	// set line color
+	ctx.strokeStyle = '#808000';
+	ctx.stroke();
+	sensLeftUi.innerText = "Sensor left: " + sensTempLeft.d;
+	
+	var sensTempRight = car_sensor("right");
+	ctx.beginPath();
+	ctx.moveTo(playerPosX, playerPosY);
+	ctx.lineTo(sensTempRight.x, sensTempRight.y);
+	ctx.lineWidth = 2;
+	// set line color
+	ctx.strokeStyle = '#808000';
+	ctx.stroke();
+	sensRightUi.innerText = "Sensor right: " + sensTempRight.d;
+	
+	ctx.save();
+	ctx.translate(playerPosX, playerPosY);
+	//
+	ctx.rotate(absoluteAngle * Math.PI / 180);
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(-10, -10, 20, 20);
+	ctx.fillStyle = "#0000FF";
+	ctx.fillRect(-10, -10, 20, 5);
+	//ctx.translate( playerPosX/2, playerPosY/2 );
+	//ctx.rotate(0);
+	ctx.restore();
+    //ctx.fillStroke(0, 0, 20, 20);
+    
+	wheelsUi.innerText = "Wheels angle: " + Math.round((angle*180)/Math.PI);
+	carAngleUi.innerText = "Car angle: " + absoluteAngle;
+	
+	carXposUi.innerText = "Car X pos: " + playerPosX;
+	carYposUi.innerText = "Car Y pos: " + playerPosY;
+	
+	carXposAbsUi.innerText = "Car X pos: " + playerAbsolutePosX;
+	carYposAbsUi.innerText = "Car Y pos: " + playerAbsolutePosY;
+
+}, 16);
+
+function acc(speed){
+	//speed = 1;
+	playerPosVY = -Math.cos(absoluteAngle * Math.PI / 180) * speed;
+	playerPosVX = Math.sin(absoluteAngle * Math.PI / 180) * speed;    
+	
+	// Plot the new velocity into x and y cords
+	//playerPosY = Math.round(playerPosY + playerPosVY);
+	//playerPosX = Math.round(playerPosX + playerPosVX);
+	
+	playerAbsolutePosY += playerPosVY;
+	playerAbsolutePosX += playerPosVX;
+	
+	absoluteAngle += angle;
+	
+	//console.log(playerPosY+":"+playerPosX);
+}
+
+function updateWheel(n){
+	wheel = n;
+}
+//30 / -30 
+function steer(dir, a){
+	//updateWheel(a);
+	if (dir == "right")
+		angle = a * Math.PI / 180;
+	else
+		angle = a * Math.PI / 180;
+}
+
+init_draw();
+draw(false, 0, 0, 0);
+
+
+function init_draw(){
+	ctx.fillStyle = "#dbdbdb";
+		ctx.fillRect(0, 0, WIDTH, HEIGHT);
+}
+testX = 0;
+textY = 0;
+
+function draw_car_sensor(){
+	//playerPosX =50;
+	//playerPosY =50;
+	//---front
+	radian = (Math.PI/180) * (absoluteAngle);
+	range = 50;
+	newX = Math.round((playerPosX) + (Math.sin(radian) * range));
+	newY = Math.round((playerPosY) - (Math.cos(radian) * range));
+	
+	console.log(newX+":"+newY+":"+radian);
+	
+	array2D[newY][newX] = "#00ff00";
+	//---left
+	radian = (Math.PI/180) * (absoluteAngle-45);
+	range = 30;
+	newX = Math.round((playerPosX) + (Math.sin(radian) * range));
+	newY = Math.round((playerPosY) - (Math.cos(radian) * range));
+	console.log(newX+":"+newY+":"+radian);	
+	array2D[newY][newX] = "#00ff00";
+	//right
+	radian = (Math.PI/180) * (absoluteAngle+45);
+	range = 30;
+	newX = Math.round((playerPosX) + (Math.sin(radian) * range));
+	newY = Math.round((playerPosY) - (Math.cos(radian) * range));
+	console.log(newX+":"+newY+":"+radian);	
+	array2D[newY][newX] = "#00ff00";
+	
+	//ctx.fillStyle = "#00ff00";
+	//ctx.fillRect(playerPosX, playerPosY, 1, 1);
+}
+
+function car_sensor(dir){
+	distance = 0;
+	range = 50;
+	radian = 0;
+	
+	
+	switch (dir) {
+		case "front":
+			radian = (Math.PI/180) * (absoluteAngle);
+			break;
+		case "left":
+			radian = (Math.PI/180) * (absoluteAngle - 45);
+			break;
+		case "right":
+			radian = (Math.PI/180) * (absoluteAngle + 45);
+			break;
+	}
+	var pathType = "#007070";
+	newX = (playerPosX);
+	newY = (playerPosY);
+	if(array2D[newY][newX] == "#007070")
+		pathType = "#dbdbdb";
+	
+	for(i = 0; i <= range; i++){
+	newX = Math.round((playerPosX) + (Math.sin(radian) * i));
+	newY = Math.round((playerPosY) - (Math.cos(radian) * i));
+	
+	if(newY < 0)
+		newY = 0;
+	
+	if(newY > HEIGHT-1)
+		newY = HEIGHT-1;
+	
+	if(newX < 0)
+		newX = 0;
+	
+	if(newX > WIDTH-1)
+		newX = WIDTH-1;
+	
+	if(array2D[newY][newX] == pathType || (newY <= 0 || newY >= HEIGHT-1 || newX <= 0 || newX >= WIDTH-1)){
+		
+		if (pathType == "#dbdbdb")
+			distance = range - Math.sqrt(Math.pow(newX - playerPosX, 2) + Math.pow(newY - playerPosY, 2));
+		else
+			distance = Math.sqrt(Math.pow(newX - playerPosX, 2) + Math.pow(newY - playerPosY, 2));
+		break;
+	}
+	
+	if(i == range)
+		if (pathType == "#dbdbdb")
+			distance = 0;
+		else
+			distance = 50;
+	}
+	
+	
+	ret = {d: distance, x: newX, y: newY};
+	
+	return ret;
+	
+	
+	//console.log(newX+":"+newY+":"+radian);
+	
+	//array2D[newY][newX] = "#00ff00";
+	// grass "#007070"; 
+	// road "#dbdbdb";
+	
+	
+	/*
+	if(array2D[newY][newX] == "#dbdbdb"){
+		return true;
+	}else{
+		return false;
+	}*/
+}
+
+function draw(update, y, x, r){
+		var time = new Date().getTime();
+		//var curTime;
+		//draw
+		//sky
+		//ctx.fillStyle = "#dbdbdb";
+		//ctx.fillRect(0, 0, WIDTH, HEIGHT);
+		if(update == false){
+			id.data[3] = 255;
+			for(var i = 0 ; i < HEIGHT; i++){
+				for(var j = 0 ; j < WIDTH; j++){
+					//ctx.fillStyle = array2D[i][j];
+					if(array2D[i][j] == "#007070"){
+						id.data[0] = "0";
+						id.data[1] = "112";
+						id.data[2] = "112";
+					}else{
+						id.data[0] = "219";
+						id.data[1] = "219";
+						id.data[2] = "219";
+					}
+					//ctx.fillRect(j, i, 1, 1);
+					ctx.putImageData(id, j, i);
+				}
+			}
+		} else {
+			id.data[3] = 255;
+			for(var i = x-r ; i < x+r; i++){
+				for(var j = y-r ; j < y+r; j++){
+					//ctx.fillStyle = array2D[i][j];
+					if(i < HEIGHT && j < WIDTH && i >= 0 && j >= 0){
+						if(array2D[i][j] == "#007070"){
+							id.data[0] = "0";
+							id.data[1] = "112";
+							id.data[2] = "112";
+						}else{
+							id.data[0] = "219";
+							id.data[1] = "219";
+							id.data[2] = "219";
+						}
+						//ctx.fillRect(j, i, 1, 1);
+						ctx.putImageData(id, j, i);
+					}
+				}
+			}
+		}
+		
+		draw_time.innerText = (new Date().getTime() - time) + " ms";
+}
+
+
+
+function yourFunction(mouseX, mouseY) {
+  console.log("Inside!");
+  console.log(mouseY+":"+mouseX);
+  var r = 20;
+  for(var i = mouseY - r; i <= mouseY + r; i++)
+	{
+	   for(var j = mouseX - r; j <= mouseX + r; j++)
+	   {
+		   //console.log(i+":"+j);
+		   //array2D[i][j] = "#dbdbdb";
+		   if((i-mouseY)*(i-mouseY) + (j-mouseX)*(j-mouseX) <= r*r)
+		   {
+			   if(i < HEIGHT && j < WIDTH && i >= 0 && j >= 0)
+					array2D[i][j] = "#dbdbdb";
+		   }
+	   }
+	}
+	draw(true, mouseX, mouseY, r);
+}
+
+tool = new tool_pencil();
+
+canvas.addEventListener('mousedown', ev_canvas, false);
+canvas.addEventListener('mousemove', ev_canvas, false);
+canvas.addEventListener('mouseup',   ev_canvas, false);
+
+function tool_pencil () {
+    var tool = this;
+    this.started = false;
+
+    this.mousedown = function (ev) {
+        yourFunction(ev._x, ev._y);
+		
+        tool.started = true;
+    };
+
+    this.mousemove = function (ev) {
+		cords.innerText = "x: "+ev._x+" y: "+ev._y;
+      if (tool.started) {
+        yourFunction(ev._x, ev._y);
+      }
+    };
+
+    this.mouseup = function (ev) {
+      if (tool.started) {
+        tool.mousemove(ev);
+        tool.started = false;
+      }
+    };
+  }
+
+function ev_canvas (ev) {
+    if (ev.layerX || ev.layerX == 0) { // Firefox
+      ev._x = ev.layerX;
+      ev._y = ev.layerY;
+    } else if (ev.offsetX || ev.offsetX == 0) { // Opera
+      ev._x = ev.offsetX;
+      ev._y = ev.offsetY;
+    }
+
+    var func = tool[ev.type];
+    if (func) {
+      func(ev);
+    }
+  }
+  
+  
+//controlls
+document.addEventListener('keydown', (e) => {
+	if (e.code === "ArrowLeft")
+		controlls("left",true);
+		//left = true;
+	else if (e.code === "ArrowRight")
+		controlls("right",true);
+		//right = true;
+	else if (e.code === "ArrowUp")
+		controlls("up",true);
+		//up = true;
+	else if (e.code === "ArrowDown")
+		controlls("down",true);
+		//down = true;
+	else if (e.code === "Space")
+		controlls("space",true);
+		//space = true;
+	
+	document.getElementById("key").innerHTML = e.code;
+});
+document.addEventListener('keyup', (e) => {
+	if (e.code === "ArrowLeft")
+		controlls("left",false);
+		//left = false;
+	else if (e.code === "ArrowRight")
+		controlls("right",false);
+		//right = false;
+	else if (e.code === "ArrowUp")
+		controlls("up",false);
+		//up = false;
+	else if (e.code === "ArrowDown")
+		controlls("down",false);
+		//down = false;
+	else if (e.code === "Space")
+		controlls("space",false);
+		//space = false;
+});
+
+
+function controlls(key, state, an){
+	switch(key){
+		case "left":
+			if(state == true){
+				customAngle = an;
+				left = true;
+			}
+			else {
+				//customAngle = 0;
+				left = false;
+			}
+			break;
+		case "right":
+			if(state == true){
+				customAngle = an;
+				right = true;
+			}
+			else {
+				//customAngle = 0;
+				right = false;
+			}
+			break;
+		case "up":
+			if(state == true)
+				up = true;
+			else
+				up = false;
+			break;
+		case "down":
+			if(state == true)
+				down = true;
+			else
+				down = false;
+			break;
+		case "space":
+			if(state == true)
+				space = true;
+			else
+				space = false;
+			break;
+		case "ai":
+			if(state == true)
+				ai = true;
+			else
+				ai = false;
+			break;
+		
+	}
+}
